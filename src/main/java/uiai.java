de264d56441bfd5b6ff02c8968c6dcb44e2966b9
xhuinn <xhwong24@gmail.com>
2025-01-22 17:04:53 +0800

    Update uiai.java level0

---
+++ src/main/java/uiai.java
@@ -0,0 +1,17 @@
+public class uiai {
+    public static void main(String[] args) {
+        String logo =
+                " /\\_/\\\n"
+                + "( o.o )\n"
+                + " > ^ <";
+
+        String divider =
+                "♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡";
+
+        System.out.println("Meow! I'm uiai, your helpful cat\n" + logo);
+        System.out.println("How can I help you?\n" + divider);
+        System.out.println("Bye! Hope you had a meow-tastic time ฅ^•⩊•^ฅ \n" + divider);
+    }
+}
+
+
