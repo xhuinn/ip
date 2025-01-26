a8731e1d6bbea1de939f70226ffef2e7e98c181a
xhuinn <xhwong24@gmail.com>
2025-01-26 14:22:35 +0800

    Add level-1 echo

--- src/main/java/uiai.java
+++ src/main/java/uiai.java
@@ -1,16 +1,32 @@
+import java.util.Scanner;
+
 public class uiai {
     public static void main(String[] args) {
         String logo =
-                " /\\_/\\\n"
-                + "( o.o )\n"
-                + " > ^ <";
+                "\t /\\_/\\\n"
+                        + "\t( o.o )\n"
+                        + "\t > ^ <";
 
         String divider =
-                "♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡";
+                "\t♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡";
 
-        System.out.println("Meow! I'm uiai, your helpful cat\n" + logo);
-        System.out.println("How can I help you?\n" + divider);
-        System.out.println("Bye! Hope you had a meow-tastic time ฅ^•⩊•^ฅ \n" + divider);
+        System.out.println("\t Meow! I'm uiai, your helpful cat\n" + logo);
+        System.out.println("\tHow can I help you?\n" + divider);
+
+        // Level-1: echo
+        boolean task = true;
+        while (task) {
+            String line;
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                System.out.println(divider + "\n" + "\tBye! Hope you had a meow-tastic time ฅ^•⩊•^ฅ \n" + divider);
+                task = false;
+            } else {
+                System.out.println(divider + "\n\t" + line + "\n" + divider);
+                line = null;
+            }
+        }
     }
 }
 
