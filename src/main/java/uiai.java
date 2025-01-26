4a39f4cdd970ee2ec2e4de82f054096b7e2dec32
xhuinn <xhwong24@gmail.com>
2025-01-26 16:30:06 +0800

    Add level-2 add, list

--- src/main/java/uiai.java
+++ src/main/java/uiai.java
@@ -13,21 +13,26 @@ public class uiai {
         System.out.println("\t Meow! I'm uiai, your helpful cat\n" + logo);
         System.out.println("\tHow can I help you?\n" + divider);
 
-        // Level-1: echo
+        String[] items = new String[100];
+        int index = 0;
+        Scanner in = new Scanner(System.in);
+
         boolean task = true;
         while (task) {
-            String line;
-            Scanner in = new Scanner(System.in);
-            line = in.nextLine();
+            String line = in.nextLine();
+
             if (line.equals("bye")) {
                 System.out.println(divider + "\n" + "\tBye! Hope you had a meow-tastic time ฅ^•⩊•^ฅ \n" + divider);
                 task = false;
+            } else if (line.equals("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println("\t" + (i + 1) + ": " + items[i]);
+                }
             } else {
-                System.out.println(divider + "\n\t" + line + "\n" + divider);
-                line = null;
+                System.out.println(divider + "\n\t" + "added: " + line + "\n" + divider);
+                items[index] = line;
+                index++;
             }
         }
     }
 }
-
-
