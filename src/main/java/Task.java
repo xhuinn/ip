991a10ea75d70c0815cae30c312e851a21a24ab3
xhuinn <xhwong24@gmail.com>
2025-02-04 10:40:34 +0800

    Add level-4 ToDos, Events, Deadlines

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -19,4 +19,9 @@ public class Task {
         isDone = false;
     }
 
+    @Override
+    public String toString() {
+        return "[ ]" + "[" + getStatusIcon() + "] " + description;
+    }
+
 }
--- src/main/java/Uiai.java
+++ src/main/java/Uiai.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 
 public class Uiai {
 
+    public static final String DIVIDER = "\t♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡";
+    public static final String LOGO = "\t /\\_/\\\n"
+            + "\t( o.o )\n"
+            + "\t > ^ <";
+
     public static void main(String[] args) {
-        String logo =
-                "\t /\\_/\\\n"
-                        + "\t( o.o )\n"
-                        + "\t > ^ <";
+        System.out.println("\t Meow! I'm Uiai, your helpful cat\n" + LOGO);
+        System.out.println("\tHow can I help you?\n" + DIVIDER);
 
-        String divider =
-                "\t♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡";
-
-        System.out.println("\t Meow! I'm Uiai, your helpful cat\n" + logo);
-        System.out.println("\tHow can I help you?\n" + divider);
-
-        ArrayList<Task> tasks = new ArrayList<>();
+        //ArrayList<Task> tasks = new ArrayList<>();
+        Task[] tasks = new Task[100];
+        int tasksIndex = 0;
 
         Scanner in = new Scanner(System.in);
         boolean task = true;
@@ -26,25 +25,25 @@ public class Uiai {
 
             switch (command[0]) {
             case "bye":
-                System.out.println(divider + "\n" + "\tBye! Hope you had a meow-tastic time ฅ^•⩊•^ฅ \n" + divider);
+                System.out.println(DIVIDER + "\n" + "\tBye! Hope you had a meow-tastic time ฅ^•⩊•^ฅ \n" + DIVIDER);
                 task = false;
                 break;
 
             case "list":
-                System.out.println(divider + "\n\tᨐฅ Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.printf("\t" + (i + 1) + ".[" + tasks.get(i).getStatusIcon() + "] " + tasks.get(i).description + "\n");
+                System.out.println(DIVIDER + "\n\tᨐฅ Here are the tasks in your list:");
+                for (int i = 0; i < tasksIndex; i++) {
+                    System.out.println("\t" + (i + 1) + "." + tasks[i].toString());
                 }
-                System.out.println(divider);
+                System.out.println("\tcurrently there's " + tasksIndex + " tasks in your list!\n" + DIVIDER);
                 break;
 
             case "mark":
                 try {
                     int markIndex = Integer.parseInt(command[1]) - 1;
-                    tasks.get(markIndex).markAsDone();
-                    System.out.println(divider + "\n\t≽^-⩊-^≼ Marked as done:");
-                    System.out.println("\t  [" + tasks.get(markIndex).getStatusIcon() + "] " + tasks.get(markIndex).description);
-                    System.out.println(divider);
+                    tasks[markIndex].markAsDone();
+                    System.out.println(DIVIDER + "\n\t≽^-⩊-^≼ Marked as done:");
+                    System.out.println("\t"  + tasks[markIndex].toString());
+                    System.out.println(DIVIDER);
                 } catch (Exception e) {
                     System.out.println("\tInvalid task number.");
                 }
@@ -53,18 +52,59 @@ public class Uiai {
             case "unmark":
                 try {
                     int unmarkIndex = Integer.parseInt(command[1]) - 1;
-                    tasks.get(unmarkIndex).markAsNotDone();
-                    System.out.println(divider + "\n\t≽^-⩊-^≼ Marked as not done yet:");
-                    System.out.println("\t  [" + tasks.get(unmarkIndex).getStatusIcon() + "] " + tasks.get(unmarkIndex).description);
-                    System.out.println(divider);
+                    tasks[unmarkIndex].markAsNotDone();
+                    System.out.println(DIVIDER + "\n\t≽^-⩊-^≼ Marked as not done yet:");
+                    System.out.println("\t"  + tasks[unmarkIndex].toString());
+                    System.out.println(DIVIDER);
                 } catch (Exception e) {
                     System.out.println("\tInvalid task number.");
                 }
                 break;
 
+            case "deadline":
+                try {
+                    String[] description = command[1].split("/by ", 2);
+                    tasks[tasksIndex] = new Deadline(description[0], description[1] );
+                    System.out.println("\tᓚ₍ ^. .^₎ added this task!");
+                    System.out.println("\t" + tasks[tasksIndex].toString());
+                    System.out.println("\tcurrently there's " + (tasksIndex + 1) + " tasks in your list!\n" + DIVIDER);
+                    tasksIndex++;
+                } catch (Exception e) {
+                    System.out.println("\tNo Deadline found.");
+                }
+                break;
+
+            case "todo":
+                try {
+                    String description = command[1];
+                    tasks[tasksIndex] = new Todo(description);
+                    System.out.println("\tᓚ₍ ^. .^₎ added this task!");
+                    System.out.println("\t" + tasks[tasksIndex].toString());
+                    System.out.println("\tcurrently there's " + (tasksIndex + 1) + " tasks in your list!\n" + DIVIDER);
+                    tasksIndex++;
+                } catch (Exception e) {
+                    System.out.println("\tNo Todo found");
+                }
+                break;
+
+            case "event":
+                try {
+                    String[] description = command[1].split("/from", 2);
+                    String[] time = description[1].split("/to", 2);
+                    tasks[tasksIndex] = new Event(description[0], time[0], time[1]);
+                    System.out.println("\tᓚ₍ ^. .^₎ added this task!");
+                    System.out.println("\t" + tasks[tasksIndex].toString());
+                    System.out.println("\tcurrently there's " + (tasksIndex + 1) + " tasks in your list!\n" + DIVIDER);
+                    tasksIndex++;
+                } catch (Exception e) {
+                    System.out.println("\tNo Event found.");
+                }
+                break;
+
             default:
-                System.out.println(divider + "\n\t" + "added: " + line + "\n" + divider);
-                tasks.add(new Task(line));
+                System.out.println(DIVIDER + "\n\t" + "ᓚ₍ ^. .^₎ added this task!\n\t " + line + "\n\t" + "currently there's " + (tasksIndex + 1) + " tasks in your list!\n" + DIVIDER);
+                tasks[tasksIndex] = new Task(line);
+                tasksIndex++;
                 break;
             }
         }
